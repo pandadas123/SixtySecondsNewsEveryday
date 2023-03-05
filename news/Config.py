584e154b1f951149989c92484d8a67ede938acef
pandadas123 <2096840950@qq.com>
2023-03-05 12:47:53 +0800

    更改配置读取方式，实时读取

--- news/Config.py
+++ news/Config.py
@@ -1,13 +1,15 @@
 import configparser
 import os
 
+config = configparser.ConfigParser()
+#      os.path.split(os.path.realpath(__file__))[0] 得到的是当前文件模块的目录
+path = os.getcwd() + os.sep + 'global.conf'
+
 
 # 获取config配置文件
 def get_config(section, key):
-    config = configparser.ConfigParser()
-
-    #      os.path.split(os.path.realpath(__file__))[0] 得到的是当前文件模块的目录
-    path = os.getcwd() + os.sep + 'global.conf'
-
+    global config, path
     config.read(path, "utf8")
-    return config.get(section, key)
+    _config = config.get(section, key)
+    config.clear()  # 清楚缓存，实时读取配置
+    return _config
