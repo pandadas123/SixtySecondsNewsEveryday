aef87c5507105dfd49b8ee1c4a19fd0aca464b65
pandadas123 <2096840950@qq.com>
2023-03-05 15:52:04 +0800

    更改任务调度执行逻辑

--- news/ScheduledTask.py
+++ news/ScheduledTask.py
@@ -1,18 +1,21 @@
 import time
+from datetime import datetime
 from typing import Callable
 
 from news import Config
 
 
 def scheduler(task: Callable):
-    """定时任务"""
+    """定时任务，需要任务在半小时内完成一次"""
     print("\n                                      --------------------------------- ")
     print("                                     |                                 |  ")
     print("                                     |            任务开始              |   ")
     print("                                     |                                 |  ")
     print("                                      ---------------------------------\n ")
     while True:
-        time_now = time.strftime("%H:%M", time.localtime())  # 刷新
-        if time_now == Config.get_config("task", "time"):  # 设置要执行的时间
-            task()
-            time.sleep(61)  # 停止执行61秒，防止反复运行程序。
+        time_task = datetime.strptime(Config.get_config("task", "time"), "%H:%M:%S")           # 获取任务时间
+        time_now = datetime.strptime(time.strftime("%H:%M:%S", time.localtime()), "%H:%M:%S")  # 获取当前时间
+        duration = (time_task - time_now).seconds  # 计算时间差
+        time.sleep(int(duration))                  # 暂停执行
+        task()  # 执行任务
+        time.sleep(23.5 * 60 * 60)                   # 在下次任务时间点前半小时开始下次循环
